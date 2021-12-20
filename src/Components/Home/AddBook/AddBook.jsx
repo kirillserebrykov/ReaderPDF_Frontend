db317e20c23ffe63defc86ea2ee43ffca6a44fc7
Kirill Serebrykov <kirill.baza22@gmail.com>
2021-12-20 23:18:54 +0300

    add btn upload file

--- src/Components/Home/AddBook/AddBook.jsx
+++ src/Components/Home/AddBook/AddBook.jsx
@@ -1,14 +1,31 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import style from './AddBook.module.css';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import NotesIcon from '@mui/icons-material/Notes';
 import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+
+
 const AddBook = () => {
+    const [drag, setDrag] = useState(false)
+
+    function dragDropStartHandler(e) {
+        e.preventDefault()
+        setDrag(true)
+    }
+    function dragDropEndHandler(e) {
+        e.preventDefault();
+        setDrag(false)
+    }
+    function dragDropHandler(e) {
+        e.preventDefault();
+        console.log([...e.dataTransfer.files])
+    }
+
     return (
         <section className={style.uploadBookPopUp} >
             <div className={style.uploadBookPopUpClose}>
@@ -28,17 +45,25 @@ const AddBook = () => {
                 </div>
             </div>
             <span className={style.uploadBookPopUpTitle}>Добавить Документ</span>
-            <div className={style.dropDownWrrap} >
-                <div className={style.dropDown} >
-
+            <div onDragStart={e => dragDropStartHandler(e)}
+                onDragLeave={e => dragDropEndHandler(e)}
+                onDragOver={e => dragDropStartHandler(e)}
+                onDrop={e => dragDropHandler(e)}
+                className={style.DragAndDropWrrap} >
+                <div
+                    className={style.DragAndDrop} >
+                    <IconButton variant="contained"component="label">
+                    <UploadFileIcon sx={{ fontSize: "60px", fill: '#4F4C4C' }} />
+                        <input type="file" hidden />
+                    </IconButton>
                 </div>
             </div>
             <div className={style.uploadBookPopUpNext}>
-            <IconButton color="success"  aria-label="upload picture" component="span">
+                <IconButton color="success" aria-label="upload picture" component="span">
                     <NavigateNextIcon sx={{ fontSize: "30px" }} />
                 </IconButton>
             </div>
-            
+
         </section>
     );
 }
