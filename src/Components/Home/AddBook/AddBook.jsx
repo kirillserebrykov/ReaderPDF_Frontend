f59bb1459f220e994dfeba476fb6ac815061ecc0
Kirill Serebrykov <kirill.baza22@gmail.com>
2021-12-24 23:23:51 +0300

    add animation in upload document

--- src/Components/Home/AddBook/AddBook.jsx
+++ src/Components/Home/AddBook/AddBook.jsx
@@ -8,9 +8,9 @@ import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
+import Collapse from '@mui/material/Collapse'
 
-
-const AddBook = () => {
+const DragAndDropWindow = ({ setFile }) => {
     const [drag, setDrag] = useState(false)
 
     function dragDropStartHandler(e) {
@@ -23,11 +23,52 @@ const AddBook = () => {
     }
     function dragDropHandler(e) {
         e.preventDefault();
-        console.log([...e.dataTransfer.files])
-    }
+        setFile([...e.dataTransfer.files])
 
+    }
+    function onFileChange(e) {
+        e.preventDefault();
+        setFile([...e.target.files])
+
+    }
+    return <div onDragStart={e => dragDropStartHandler(e)}
+        onDragLeave={e => dragDropEndHandler(e)}
+        onDragOver={e => dragDropStartHandler(e)}
+        onDrop={e => dragDropHandler(e)}
+        className={style.DragAndDropWrrap} >
+        <div className={style.DragAndDrop} >
+            <IconButton variant="contained" component="label">
+                <UploadFileIcon sx={{ fontSize: "60px", fill: '#4F4C4C' }} />
+                <input multiple accept=".pdf" onChange={onFileChange} type="file" hidden />
+            </IconButton>
+        </div>
+
+    </div>
+
+}
+
+
+
+const SelectDocs = ({ files }) => {
+
+    const filesName = files.map((file, index) => {
+        const currentIndex = index + 1
+        const LastFile = currentIndex === files.length
+        return file.name + `${LastFile ? "" : ", "}`
+    })
+    return  <div className={style.filesNameWrrap}>
+                <span className={style.filesName}>{filesName}</span>
+            </div>
+}
+
+
+
+
+const AddBook = () => {
+    const [files, setFile] = useState([])
+    const isfiles = files[0]
     return (
-        <section className={style.uploadBookPopUp} >
+        <section className={style.uploadBookPopUp}>
             <div className={style.uploadBookPopUpClose}>
                 <IconButton sx={{ color: "#4F4C4C", }} aria-label="upload picture" component="span">
                     <CloseIcon sx={{ fontSize: "30px" }} />
@@ -35,7 +76,7 @@ const AddBook = () => {
             </div>
             <div className={style.uploadBookPopUpProcess}>
                 <div className={`${style.uploadBookPopUpProcessActions} ${style.Upload}`}>
-                    <FileUploadIcon sx={{ fontSize: "30px", fill: '#4F4C4C' }} />
+                    <FileUploadIcon sx={isfiles ? { fontSize: "30px", fill: '#2e7d32' } : { fontSize: "30px", fill: '#4F4C4C' }} />
                 </div>
                 <div className={`${style.uploadBookPopUpProcessActions} ${style.MetaData}`}>
                     <NotesIcon sx={{ fontSize: "30px", fill: '#4F4C4C' }} />
@@ -44,26 +85,15 @@ const AddBook = () => {
                     <DoneOutlineIcon sx={{ fontSize: "30px", fill: '#4F4C4C' }} />
                 </div>
             </div>
-            <span className={style.uploadBookPopUpTitle}>Добавить Документ</span>
-            <div onDragStart={e => dragDropStartHandler(e)}
-                onDragLeave={e => dragDropEndHandler(e)}
-                onDragOver={e => dragDropStartHandler(e)}
-                onDrop={e => dragDropHandler(e)}
-                className={style.DragAndDropWrrap} >
-                <div
-                    className={style.DragAndDrop} >
-                    <IconButton variant="contained"component="label">
-                    <UploadFileIcon sx={{ fontSize: "60px", fill: '#4F4C4C' }} />
-                        <input type="file" hidden />
-                    </IconButton>
-                </div>
-            </div>
+            <span className={style.uploadBookPopUpTitle}>{!isfiles ? "Загрузить Документ" : "Загруженые Документ"}</span>
+            <Collapse sx={{ width: "100%" }} in={Boolean(!isfiles)}>{<DragAndDropWindow setFile={setFile} />}</Collapse>
+            <Collapse in={Boolean(isfiles)}>{<SelectDocs files={files} />}</Collapse>
             <div className={style.uploadBookPopUpNext}>
                 <IconButton color="success" aria-label="upload picture" component="span">
                     <NavigateNextIcon sx={{ fontSize: "30px" }} />
                 </IconButton>
             </div>
-
+        
         </section>
     );
 }
